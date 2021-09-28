f622b87b18cbd2a8761607248c289ff300976926
Chinmay Kousik <ckousik@littlebearlabs.io>
2021-09-28 14:56:06 +0000

    improve peerconnection observer

--- src/peer_connection.rs
+++ src/peer_connection.rs
@@ -6,25 +6,23 @@ use libwebrtc::peerconnection::{
     IceServer, PeerConnection as WebRtcPeerConnection, RTCConfiguration,
 };
 use libwebrtc::peerconnection_factory::PeerConnectionFactory;
-use libwebrtc::peerconnection_observer::{
-    IceConnectionState, PeerConnectionObserver, PeerConnectionObserverTrait,
-};
+use libwebrtc::peerconnection_observer::{PeerConnectionObserver, PeerConnectionObserverTrait};
 use libwebrtc::stats_collector::{DummyRTCStatsCollector, RTCStatsCollectorCallback};
 use std::collections::VecDeque;
 use std::sync::mpsc::channel;
 use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc::{Receiver, Sender};
-use tracing::{debug, info, warn};
+use tracing::{info, warn};
 
-pub(crate) struct PeerConnection {
+pub(crate) struct PeerConnection<T: PeerConnectionObserverTrait> {
     pub(crate) id: String,
     pub(crate) name: String,
     pub(crate) webrtc_peer_connection: WebRtcPeerConnection,
-    pub(crate) holder: ChannelPCObsPtr<ChannelPeerConnectionObserver>,
+    pub(crate) observer: PeerConnectionObserver<T>,
     pub(crate) receiver: Receiver<String>,
 }
 
-impl fmt::Debug for PeerConnection {
+impl<T: PeerConnectionObserverTrait> fmt::Debug for PeerConnection<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "id={}, name={}", self.id, self.name)
     }
@@ -40,43 +38,22 @@ pub(crate) struct PeerConnectionQueueInner {
 /// Queue to fill on the gRPC side to be consumed by the websocket
 pub(crate) type PeerConnectionQueue = VecDeque<PeerConnectionQueueInner>;
 
-#[allow(dead_code)]
-pub(crate) struct ChannelPCObsPtr<T: PeerConnectionObserverTrait> {
-    pub(crate) _ptr: *mut T,
-}
-
-unsafe impl<T: PeerConnectionObserverTrait> Send for ChannelPCObsPtr<T> {}
-unsafe impl<T: PeerConnectionObserverTrait> Sync for ChannelPCObsPtr<T> {}
-
 #[derive(Clone)]
 pub(crate) struct ChannelPeerConnectionObserver {
     sender: Sender<String>,
 }
 
 impl ChannelPeerConnectionObserver {
-    pub(crate) fn new(sender: Sender<String>) -> Box<Self> {
-        Box::new(Self { sender })
-    }
-
-    #[allow(dead_code)]
-    pub(crate) fn drop_ref(obs: *mut Self) {
-        unsafe { Box::from_raw(obs) };
-
-        // drop here
-
-        debug!("peerconnection observer dropped");
+    pub(crate) fn new(sender: Sender<String>) -> Self {
+        Self { sender }
     }
 }
 
 impl PeerConnectionObserverTrait for ChannelPeerConnectionObserver {
-    fn on_standardized_ice_connection_change(&mut self, state: IceConnectionState) {
-        info!("new state: {:?}", state);
-    }
-
-    fn on_ice_candidate(&mut self, candidate_sdp: String, sdp_mid: String, sdp_mline_index: u32) {
+    fn on_ice_candidate(&mut self, candidate_sdp: &str, sdp_mid: &str, sdp_mline_index: u32) {
         info!("candidate generated: {} {}", sdp_mid, sdp_mline_index);
 
-        match self.sender.blocking_send(candidate_sdp) {
+        match self.sender.blocking_send(candidate_sdp.to_owned()) {
             Err(_err) => {
                 warn!("could not pass sdp candidate");
             }
@@ -85,17 +62,14 @@ impl PeerConnectionObserverTrait for ChannelPeerConnectionObserver {
     }
 }
 
-impl PeerConnection {
+impl<T> PeerConnection<T> {
     pub(crate) async fn new(
         peer_connection_factory: &PeerConnectionFactory,
         id: String,
         name: String,
     ) -> Result<PeerConnection> {
         let (tx, rx) = tokio::sync::mpsc::channel::<String>(10);
-        let holder = ChannelPCObsPtr {
-            _ptr: Box::into_raw(ChannelPeerConnectionObserver::new(tx)),
-        };
-        let observer = PeerConnectionObserver::new(holder._ptr)
+        let observer = PeerConnectionObserver::new(ChannelPeerConnectionObserver::new(tx))
             .map_err(|e| ServerError::CreatePeerConnectionError(e.to_string()))?;
 
         let webrtc_peer_connection = peer_connection_factory
@@ -106,7 +80,7 @@ impl PeerConnection {
             id,
             name,
             webrtc_peer_connection,
-            holder,
+            observer,
             receiver: rx,
         })
     }
