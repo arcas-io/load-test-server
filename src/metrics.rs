d58a2542d73b2e8645dad9360da5b7d0172111a9
Chinmay Kousik <chinmaykousik1@gmail.com>
2021-11-17 17:12:27 +0530

    fix metrics

--- src/metrics.rs
+++ src/metrics.rs
@@ -21,35 +21,39 @@ pub fn write_video_rx_stats(stat: &ArcasVideoReceiverStats, pc_id: &str, sess_id
         &format!("ssrc: {}", stat.ssrc),
     ];
 
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.rx.packets_received",
-        stat.packets_received as i64,
+        stat.packets_received.to_string(),
         tags,
     );
-    let _ = METRICS.count("pc.video.rx.packets_lost", stat.packets_lost as i64, tags);
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
+        "pc.video.rx.packets_lost",
+        stat.packets_lost.to_string(),
+        tags,
+    );
+    let _ = METRICS.gauge(
         "pc.video.rx.packets_repaired",
-        stat.packets_repaired as i64,
+        stat.packets_repaired.to_string(),
         tags,
     );
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.rx.bytes_received",
-        stat.bytes_received as i64,
+        stat.bytes_received.to_string(),
         tags,
     );
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.rx.frames_decoded",
-        stat.frames_decoded as i64,
+        stat.frames_decoded.to_string(),
         tags,
     );
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.rx.keyframes_decoded",
-        stat.keyframes_decoded as i64,
+        stat.keyframes_decoded.to_string(),
         tags,
     );
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.rx.frames_dropped",
-        stat.frames_dropped as i64,
+        stat.frames_dropped.to_string(),
         tags,
     );
     let _ = METRICS.gauge(
@@ -76,16 +80,16 @@ pub fn write_video_tx_stats(stat: &ArcasVideoSenderStats, pc_id: &str, sess_id: 
         &format!("ssrc: {}", stat.ssrc),
     ];
 
-    let _ = METRICS.count("pc.video.tx.packets_sent", stat.packets_sent as i64, tags);
-    let _ = METRICS.count("pc.video.tx.bytes_sent", stat.bytes_sent as i64, tags);
-    let _ = METRICS.count(
+    let _ = METRICS.gauge("pc.video.tx.packets_sent", stat.packets_sent.to_string(), tags);
+    let _ = METRICS.gauge("pc.video.tx.bytes_sent", stat.bytes_sent.to_string(), tags);
+    let _ = METRICS.gauge(
         "pc.video.tx.frames_encoded",
-        stat.frames_encoded as i64,
+        stat.frames_encoded.to_string(),
         tags,
     );
-    let _ = METRICS.count(
+    let _ = METRICS.gauge(
         "pc.video.tx.keyframes_encoded",
-        stat.key_frames_encoded as i64,
+        stat.key_frames_encoded.to_string(),
         tags,
     );
 
@@ -115,12 +119,12 @@ pub fn write_video_tx_stats(stat: &ArcasVideoSenderStats, pc_id: &str, sess_id: 
         tags,
     );
 
-    let _ = METRICS.count("pc.video.tx.nack_count", stat.nack_count as i64, tags);
-    let _ = METRICS.count("pc.video.tx.fir_count", stat.fir_count as i64, tags);
-    let _ = METRICS.count("pc.video.tx.pli_count", stat.pli_count as i64, tags);
-    let _ = METRICS.count(
+    let _ = METRICS.gauge("pc.video.tx.nack_count", stat.nack_count.to_string(), tags);
+    let _ = METRICS.gauge("pc.video.tx.fir_count", stat.fir_count.to_string(), tags);
+    let _ = METRICS.gauge("pc.video.tx.pli_count", stat.pli_count.to_string(), tags);
+    let _ = METRICS.gauge(
         "pc.video.tx.remote_packets_lost",
-        stat.remote_packets_lost as i64,
+        stat.remote_packets_lost.to_string(),
         tags,
     );
 
